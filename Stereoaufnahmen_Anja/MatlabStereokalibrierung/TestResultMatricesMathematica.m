6caedafbf7851f611adaf6be432da96f48a7dbab
Unknown <anja.siegrid.kretschmer@hs-furtwangen.de>
2018-03-09 18:27:41 +0100

    Implemented first step of scene reconstruction --> DLT pending

--- Stereoaufnahmen_Anja/MatlabStereokalibrierung/TestResultMatricesMathematica.m
+++ Stereoaufnahmen_Anja/MatlabStereokalibrierung/TestResultMatricesMathematica.m
@@ -1,6 +1,15 @@
-MathematicaP3RightHanded = [0.998795	-0.0166209	-0.0461764	-0.999019
-0.0125832	-0.82272	0.568307	-0.0442335
--0.047436	-0.568204	-0.82152	-0.00192209
+MathematicaP3RightHanded = [0.957173	-0.177379	0.228814	0.972019
+-0.203586	0.149544	0.967569	-0.234855
+-0.205844	-0.972714	0.107027	-0.00474421;
+0 0 0 1];
+
+MP3=[MathematicaP3RightHanded(1,1) MathematicaP3RightHanded(1,2) MathematicaP3RightHanded(1,3);
+    MathematicaP3RightHanded(2,1) MathematicaP3RightHanded(2,2) MathematicaP3RightHanded(2,3)
+    MathematicaP3RightHanded(3,1) MathematicaP3RightHanded(3,2) MathematicaP3RightHanded(3,3)].';
+
+MathematicaP3 =[MP3(1,1) MP3(1,2) MP3(1,3) MathematicaP3RightHanded(1,4);
+    MP3(2,1) MP3(2,2) MP3(2,3) MathematicaP3RightHanded(2,4);
+    MP3(3,1) MP3(3,2) MP3(3,3) MathematicaP3RightHanded(3,4);
 0 0 0 1];
 
 MathematicaP2LeftHanded = [-0.997455	0.0637154	-0.031995	0.999388
@@ -8,10 +17,10 @@ MathematicaP2LeftHanded = [-0.997455	0.0637154	-0.031995	0.999388
 0.0671395	0.990406	-0.120786	-0.00120798
 0 0 0 1];
 
-MathematicaP3 =  [0.997455	-0.0637154	0.031995	0.999388
--0.0239921	0.122626	0.992163	-0.0349693
--0.0671395	-0.990406	0.120786	-0.00120798
-0 0 0 1];
+%MathematicaP3 =  [0.997455	-0.0637154	0.031995	0.999388
+%-0.0239921	0.122626	0.992163	-0.0349693
+%-0.0671395	-0.990406	0.120786	-0.00120798
+%0 0 0 1];
 
 MatlabP2 =  [    0.5974   -0.6060   -0.5253   -0.7729
     0.6610    0.0013    0.7504   -0.0356
@@ -25,9 +34,12 @@ K2 = [18.60732121 0 4.145650968 0; 0 18.58796099 3.22706539 0; 0 0 1 0;0 0 0 1];
 
  
 M=[1 0 0;0 1 0;0 0 1].'*[1 0 0 0;0 1 0 0;0 0 1 0];
-M=[M(1,1) M(1,2) M(1,3) M(1,4);M(2,1) M(2,2) M(2,3) M(2,4);M(3,1) M(3,2) M(3,3) M(3,4);0 0 0 1];
+M=[M(1,1) M(1,2) M(1,3) M(1,4);
+    M(2,1) M(2,2) M(2,3) M(2,4);
+    M(3,1) M(3,2) M(3,3) M(3,4);
+    0 0 0 1];
 PMK1 =K1*M;
-PMK2= K2*MathematicaP3RightHanded;
+PMK2= K2*MathematicaP3;
 MatlabPMK2= K2*MatlabP2;
 
 
@@ -43,18 +55,19 @@ h=[1; 1; 1; 1];
 PL1 = [a b c d e f g h];
 PL2 = [a b c d e f g h];
 
-Test=zeros(4, 8);
+TestK2=zeros(4, 8);
 TestMatlab=zeros(4, 8);
 TestK1=zeros(4, 8);
 
 
 for i=1:8
 %Test(:,i) = MathematicaP3RightHanded*PL1(:,i);
-Test(:,i) = PMK2*PL1(:,i);
-Test(:,i) = Test(:,i)/Test(4,i);
+%Test(:,i) = MathematicaP3RightHanded*PL1(:,i);
+TestK1(:,i) = M*PL1(:,i);
+TestK2(:,i) = MathematicaP3*TestK1(:,i);
 
-TestK1(:,i) = PMK1*PL1(:,i);
 TestK1(:,i) = TestK1(:,i)/TestK1(4,i);
+TestK2(:,i) = TestK2(:,i)/TestK2(4,i);
 
 %TestMatlab(:,i) = MatlabP2*PL2(:,i);
 TestMatlab(:,i) = MatlabPMK2*PL2(:,i);
@@ -67,7 +80,7 @@ figure
 subplot(1,2,1)       % add first plot in 2 x 1 grid
 %plot3([PL1(1,:),PL1(1,1)],[PL1(2,:),PL1(2,1)],[PL1(3,:),PL1(3,1)]);hold on
 plot3([TestK1(1,:),TestK1(1,1)],[TestK1(2,:),TestK1(2,1)],[TestK1(3,:),TestK1(3,1)]);hold on
-plot3([Test(1,:),Test(1,1)],[Test(2,:),Test(2,1)],[Test(3,:),Test(3,1)]);
+plot3([TestK2(1,:),TestK2(1,1)],[TestK2(2,:),TestK2(2,1)],[TestK2(3,:),TestK2(3,1)]);
 xlabel('x');
 ylabel('y');
 zlabel('z');
